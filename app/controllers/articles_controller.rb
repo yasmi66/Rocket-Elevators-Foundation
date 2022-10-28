8a2491cc78427176e06ebef96bf7e6ed86c6d85d
Edouard Labrosse <edouard_labrosse@outlook.com>
2022-10-28 16:54:50 -0400

    JS fix to erase the other fields if the building type change

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,2 @@
 class ArticlesController < ApplicationController
-
-    if (nbroffloors.params == [R]) {
-        return nbroffloors[R].value
-
-    } elsif (nbroffloors.params == [C]) {
-        return nbroffloors[C].value
-    }
-
 end
