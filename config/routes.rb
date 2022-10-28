e5e78e27ad52260b84e40675e34b541d609b3e55
Edouard Labrosse <edouard_labrosse@outlook.com>
2022-10-28 13:40:44 -0400

    Frontend working

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root "articles#main-landing-page"
+  root "application#main-landing-page"
 
-  get "/main-landing-page", to: "articles#main-landing-page"
-  get "/buisness-services", to: "articles#buisness-services"
-  get "/request-quote-page", to: "articles#request-quote-page"
-  get "/residential-services", to: "articles#residential-services"
+  get "/main-landing-page", to: "application#main-landing-page"
+  get "/buisness-services", to: "application#buisness-services"
+  get "/request-quote-page", to: "application#request-quote-page"
+  get "/residential-services", to: "application#residential-services"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
