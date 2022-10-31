3d06f35b04ebb863030c8666edcd86a1639279cb
Edouard Labrosse <edouard_labrosse@outlook.com>
2022-10-31 18:16:16 -0400

    Fixed the admin button

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2022_10_27_192005) do
 
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.string "lastName"
     t.string "firstName"
