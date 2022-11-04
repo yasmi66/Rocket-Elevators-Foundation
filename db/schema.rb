4e33d3b72d9e93af9eb3f552cd70b886339ed2fb
Michael Lapierre-Ouellette <112026857+mlouellette@users.noreply.github.com>
2022-11-04 10:49:54 -0400

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -129,17 +129,17 @@ ActiveRecord::Schema.define(version: 2022_11_01_183122) do
   end
 
 
-  create_table "leads", force: :cascade do |t|
-    t.string "contactName", limit: 99, null: false
-    t.string "contactBuisnessName", limit: 99, null: false
-    t.string "contactEmail", limit: 99, null: false
-    t.string "contactPhone", null: false
-    t.string "contactProject", limit: 99, null: false
-    t.string "project_description"
-    t.string "contactDepartement", limit: 99, null: false
-    t.string "contactMessage"
-    t.binary "contactAttachment"
-    t.date "contactDate", null: false
+  create_table "leads", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+    t.string "full_name_contact", limit: 99, null: false
+    t.string "company_name", limit: 99, null: false
+    t.string "email", limit: 99, null: false
+    t.integer "phone", null: false
+    t.string "project_name", limit: 99, null: false
+    t.string "project_description", null: false
+    t.string "departement_in_charge", limit: 99, null: false
+    t.string "message"
+    t.binary "attached_file", limit: 1
+    t.date "date_contact_request", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
