01d4dc19073fc847da8b97f99bc75b990727db89
carojas14 <carorojasdevia@gmail.com>
2022-11-17 11:30:36 -0500

    graphql update

---
+++ app/graphql/types/address_type.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Types
+  class AddressType < Types::BaseObject
+    field :id, ID, null: false
+    field :type_address, String, null: false
+    field :status, String, null: false
+    field :entity, String, null: false
+    field :number_and_street, String, null: false
+    field :suit_or_apartment, String
+    field :city, String, null: false
+    field :postal_code, String, null: false
+    field :country, String, null: false
+    field :notes, String
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+  end
+end
